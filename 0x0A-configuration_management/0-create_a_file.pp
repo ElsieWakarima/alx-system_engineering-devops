036cf1939a09dd46558faf93f11c1a95e5edfa9c
Elsie Wakarima <elsiewakarima52@gmail.com>
2023-03-26 10:08:47 -0700

    Creating a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-# Create a file in /tmp
+# Creating a file in /tmp
 file {'/tmp/school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet';
 }
