fba197626a0bae4f22e54dd33186ee2cd0956c04
ElsieWakarima <96049425+ElsieWakarima@users.noreply.github.com>
2023-03-26 20:41:13 +0300

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # Create a file in /tmp
-file {'/tmp/school':
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+file { '/tmp/school':
+  ensure => file,
+  mode   => '0744',
+  owner  => 'www-data',
+  group  => 'www-data',
+  content => 'I love Puppet',
 }
