f225d35087a69bf74c32ea1f13be6c98923c0c2d
ElsieWakarima <96049425+ElsieWakarima@users.noreply.github.com>
2023-04-25 17:39:10 +0300

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,21 +4,24 @@ This script returns information about his/her TODO list progress
 using a REST API, for a given employee ID,
 """
 import requests
+import sys
 
-response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={1}')
-if response.status_code == 200:
-    todo_data = response.json()
-    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{1}')
-    employee_data = response.json()
-    emp_name = employee_data['name']
-    num_completed_tasks = 0
-    for task in todo_data:
-        if task['completed']:
-            num_completed_tasks += 1
-    total_tasks = len(todo_data)
-    print(f'Employee {emp_name} is done with {num_completed_tasks}/{total_tasks} tasks:')
-    for task in todo_data:
-        if task['completed']:
-            print(f'\t{task["title"]}')
+if __name__ == '__main__':
+    def get_employee_todo_progress(emp_id):
+        response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={emp_id}')
+        if response.status_code == 200:
+            todo_data = response.json()
+            response = requests.get(f'https://jsonplaceholder.typicode.com/users/{emp_id}')
+            employee_data = response.json()
+            emp_name = employee_data['name']
+            num_completed_tasks = 0
+            for task in todo_data:
+                if task['completed']:
+                    num_completed_tasks += 1
+            total_tasks = len(todo_data)
+            print(f'Employee {emp_name} is done with {num_completed_tasks}/{total_tasks} tasks:')
+            for task in todo_data:
+                if task['completed']:
+                    print(f'\t{task["title"]}')
 
-
+    get_employee_todo_progress(sys.argv[1])
