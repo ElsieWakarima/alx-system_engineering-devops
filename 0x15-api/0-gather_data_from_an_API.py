639ad57c5f3945c660aa264947811d3aa1c10020
ElsieWakarima <96049425+ElsieWakarima@users.noreply.github.com>
2023-04-25 17:26:32 +0300

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,28 +4,23 @@ This script returns information about his/her TODO list progress
 using a REST API, for a given employee ID,
 """
 
-import re
 import requests
-import sys
 
-REST_API = "https://jsonplaceholder.typicode.com"
+def get_employee_todo_progress(emp_id):
+    response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={emp_id}')
+    if response.status_code == 200:
+        todo_data = response.json()
+        response = requests.get(f'https://jsonplaceholder.typicode.com/users/{emp_id}')
+        employee_data = response.json()
+        emp_name = employee_data['name']
+        num_completed_tasks = 0
+        for task in todo_data:
+            if task['completed']:
+                num_completed_tasks += 1
+        total_tasks = len(todo_data)
+        print(f'Employee {emp_name} is done with {num_completed_tasks}/{total_tasks} tasks:')
+        for task in todo_data:
+            if task['completed']:
+                print(f'\t{task["title"]}')
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            emp_req = requests.get('{}/users/{}'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = emp_req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    emp_name,
-                    len(completed_tasks),
-                    len(tasks)
-                )
-            )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+get_employee_todo_progress(1)
