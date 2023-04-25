0d52c498ae9c36e7716dce77d8c64e672b077e03
ElsieWakarima <96049425+ElsieWakarima@users.noreply.github.com>
2023-04-25 15:53:37 +0300

    dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script returns information about his/her TODO list progress
+using a REST API, for a given employee ID,
+"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users"
+    employees = requests.get(url).json()
+    url = "https://jsonplaceholder.typicode.com/todos"
+    tasks = requests.get(url).json()
+    task_dict = {}
+    for employee in employees:
+        new_list = []
+        for task in tasks:
+            new_dict = {}
+            if task.get('userId') == employee.get('id'):
+                new_dict['username'] = employee.get('username')
+                new_dict['task'] = task.get('title')
+                new_dict['completed'] = task.get('completed')
+                new_list.append(new_dict)
+
+        task_dict[employee.get('id')] = new_list
+
+    with open('todo_all_employees.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(task_dict))
